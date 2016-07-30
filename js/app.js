b6a7ba9b2c02071a31cce84b662191c33371fd4b
Karla Aguilar <kn.aguilar7@gmail.com>
2016-07-29 20:53:49 -0400

    feat: add functions to Player

--- js/app.js
+++ js/app.js
@@ -24,13 +24,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+}
 
+Player.prototype.update = function() {
+    //moves around
+}
+
+Player.prototype.render = function() {
+    //updates around
+}
+
+Player.prototype.handleInput = function() {
+    //handles movement
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var bug1 = new Enemy();
+var bug2 = new Enemy();
 
+var allEnemies = [bug1, bug2];
 
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
